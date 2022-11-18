a8967095466ae552551f6cc3967586a894992c35
Omer Can Yigit <62876847+omercanyigit1@users.noreply.github.com>
2022-11-19 00:31:48 +0300

    update

--- src/constants/firebase.js
+++ src/constants/firebase.js
@@ -3,13 +3,13 @@ import "firebase/firestore";
 import "firebase/auth";
 
 var firebaseConfig = {
-    apiKey: "AIzaSyBNZdIUQ3B-b-1mx3Rq_UXQUXxg_JdDyqk",
-    authDomain: "cooper-todo.firebaseapp.com",
-    projectId: "cooper-todo",
-    storageBucket: "cooper-todo.appspot.com",
-    messagingSenderId: "567719159921",
-    appId: "1:567719159921:web:0ac74584398558633f93d0",
-    measurementId: "G-7DWRCRQ84X"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 // Initialize Firebase
